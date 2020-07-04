6020c7649dd6225e8a09df6cedaae76e3c602b38
Robin <robin98.karlsson@gmail.com>
2020-07-05 00:10:20 +0300

    Add delete post

--- lunch/src/components/AdminPanel/AdminPosts.vue
+++ lunch/src/components/AdminPanel/AdminPosts.vue
@@ -1,40 +1,52 @@
 <template>
-	<div class="admin-post-container">
-		<b-list-group v-bind:key="item.id" v-for="item in newsArr">
-			<b-list-group-item>{{item.title}}</b-list-group-item>
-		</b-list-group>
-	</div>
+  <div class="admin-post-container">
+    <b-list-group v-bind:key="item.id" v-for="item in newsArr">
+      <b-list-group-item>
+        {{item.title}}
+        <b-icon-trash class="float-right" icon="trash" @click="deletePost(item._id)"></b-icon-trash>
+      </b-list-group-item>
+    </b-list-group>
+  </div>
 </template>
 
 <script>
-	import RequetsService from '../../RequestService';
+import RequestService from "../../RequestService";
+import { BIconTrash } from "bootstrap-vue";
 
-  export default {
-    name: 'AdminPanel',
-    components: {
-        
-		},
-		data() {
-        return {
-            newsArr: []
-        }
-    },
-    async created() {
-        try {
-            this.newsArr = await RequetsService.getRequest('posts');
-            console.log(this.newsArr)
-        } catch(err) {
-            console.log(err);
-        }
-    },
-    methods: {
-
+export default {
+  name: "AdminPanel",
+  components: {
+    BIconTrash
+  },
+  data() {
+    return {
+      newsArr: []
+    };
+  },
+  async created() {
+    try {
+      this.newsArr = await RequestService.getRequest("posts");
+      console.log(this.newsArr);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  methods: {
+    deletePost(id) {
+      RequestService.deleteRequest("posts", id)
+        .then(() => {
+          this.newsArr = this.newsArr.filter(item => item._id !== id);
+        })
+        .catch(error => {
+          console.log(error.response);
+        });
     }
   }
+};
 </script>
 
 <style scoped>
-	.admin-post-container {
-		text-align: left;
-	}
+.admin-post-container {
+  text-align: left;
+}
 </style>
