38b77a60bb8156abda2bc264b22d8a036183b284
Jan Nyberg <jan.nyberg@aalto.fi>
2020-07-05 04:26:37 +0300

    Added logout to admin panel

--- lunch/src/App.vue
+++ lunch/src/App.vue
@@ -1,12 +1,55 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/admin">Admin</router-link>
+      <div class="nav-flex"></div>
+      <div class="link-container">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/admin">Admin</router-link>
+      </div>
+      <div class="nav-flex">
+        <b-button v-if="showLogout && isAuthenticated()" @click="logout()" variant="outline-info" class="mb-2 logout-button">
+          <b-icon-power icon="power" aria-hidden="true"></b-icon-power> Logout
+        </b-button>
+      </div>
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+import { BIconPower } from "bootstrap-vue";
+
+export default {
+  components: {
+      BIconPower
+  },
+  data() {
+    return {
+      showLogout: false
+    }
+  },
+  methods: {
+    isAuthenticated() {
+      return localStorage.getItem('token') !== null;
+    },
+
+    logout() {
+      localStorage.removeItem('token');
+      this.$router.go();
+    }
+  },
+  watch:{
+    $route (to) {
+        if (to.name == "Admin") {
+          console.log(to);
+          this.showLogout = true;
+        } else {
+          this.showLogout = false;
+        }
+    }
+  } 
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -19,6 +62,7 @@
 
 #nav {
   padding: 30px;
+  display: flex;
 
   a {
     font-weight: bold;
@@ -28,5 +72,17 @@
       color: #42b983;
     }
   }
+}
+
+.link-container {
+  justify-content: center;
+}
+
+.logout-button {
+  float: right;
+}
+
+.nav-flex {
+  flex: 1;
 }
 </style>
