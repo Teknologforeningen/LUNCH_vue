e1b2937b1bf4cee0bbdf243375e9f6594d8a58e0
Jan Nyberg <jan.nyberg@aalto.fi>
2020-09-13 21:15:26 +0300

    starting with styling, first graphics addes

--- lunch/src/views/Home.vue
+++ lunch/src/views/Home.vue
@@ -1,8 +1,14 @@
 <template>
-  <div id="app">
-    <h1>Testing</h1>
-    <OpeningHours />
-    <NewsList />
+  <div class="app-content-conatiner">
+    <div class="app-content-left">
+      <div class="left-image-container"></div>
+    </div>
+    <div class="app-content">
+      <h1>Testing</h1>
+      <OpeningHours />
+      <NewsList />
+    </div>
+    <div class="app-content-right"></div>
   </div>
 </template>
 
@@ -17,3 +23,41 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+  .app-content-conatiner {
+    display: flex;
+    flex-direction: row;
+    overflow: auto;
+    flex-wrap: wrap;
+  }
+
+  .app-content-left {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+  }
+
+  .left-image-container {
+    width: 300px;
+    background-size: auto 300px;
+    background-image: url(../assets/taffa_fruits.png);
+    background-repeat: repeat-y;
+  }
+
+  .app-content-right {
+    flex: 1;
+  }
+
+  @media (max-width: 1085px) {
+    .app-content-right,
+    .app-content-left  {
+      display: none;
+    }
+    .app-content-conatiner {
+      justify-content: center;
+    }
+  }
+
+
+  
+</style>
